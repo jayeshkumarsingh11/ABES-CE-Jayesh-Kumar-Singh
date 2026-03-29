49977717618798f107ae71d2a661a6235a13a054
jayeshkumarsingh11 <jayeshkumarsingh11@gmail.com>
2026-03-29 23:19:36 +0530

    backend

--- Express/App.js
+++ Express/App.js
@@ -29,100 +29,146 @@
 // })
 
 
+// const express = require('express');
+// const app = express();
+// const port = 8080;
+
+// //Database of JSON
+// const datas = [
+//     {
+//         id: 1,
+//         Name: "abc",
+//         Course: "B-Tech"
+//     },
+//     {
+//         id: 2,
+//         Name: "xyz",
+//         Course: "MBA"
+//     }
+// ]
+
+// // Data Read
+// app.get('/read', (req, res) => {
+//     try{
+//         res.status(200).json({message: "Show All Data", data: datas});
+//     } catch(err){
+//         res.status(500).json({message: "Data Not Found", error: err.message})
+//     }
+// })
+
+// //Data Read By Id
+// // This Process is Known as Params
+// app.get('/about/:id', (req, res) => {
+//     try{
+//         const id = req.params.id;
+//         const data = datas.find(item => item.id == id);
+//         if (!data){
+//             res.status(404).json({message: "Data Not Fount", d : datas});
+//         } else {
+//             res.status(200).json({message: "Data Found", data});
+//         }
+//     } catch(err) {
+//         res.status(500).json({message: "Data Not Fount", error: err.message});
+//     }
+// })
+
+// //To Create Data app.post Method is used
+// //Data Created
+// app.use(express.json());
+// app.post('/create', (req, res) => {
+//     try{
+//         const newData ={
+//             id: datas.length + 1,
+//             ...req.body
+//         }
+//         datas.push(newData);
+//         res.status(200).json({message: "Data Added", newData});
+//     } catch(err){
+//         res.status(500).json({message: "Data Not Fount", error: err.message});
+//     }
+// })
+
+// //To Update Data app.put Method is Used
+// //Data Update
+// app.put('/update/:id', (req, res) => {
+//     try{
+//         const id = req.params.id;
+//         const data = datas.findIndex(item => item.id == id);
+//         if (data === -1){
+//             return res.status(404).json({message: "Data Not Found"})
+//         }
+//         datas[data] = {
+//             ...datas[data],
+//             ...req.body
+//         }
+//         res.status(200).json({message: "Data Updated", d: datas[data]});   
+//     } catch (err){
+//         res.status(500).json({message: "Data Not Found", error: err.message});
+//     }
+// })
+
+// //To Delete Data app.delete Method is Used
+// //Data Delete
+// app.delete('/delete/:id', (req, res) => {
+//     try{
+//         const id = req.params.id;
+//         const data = datas.find(item => item.id == id);
+//         if (data == -1){
+//             return res.status(404).json({message: "Data Not Found"});
+//         }
+//         datas.splice(data, 1);
+//         res.status(200).json({message: "Data Deleted", data});
+//     } catch(err){
+//         res.status(500).json({message: "Data Not Found", error: err.message});
+//     }
+// })
+
+// app.listen(port, () => {
+//     console.log(`Server is Running at http://localhost:${port}`);
+// });
+
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8000;
 
-//Database of JSON
-const datas = [
+const students = [
     {
-        id: 1,
-        Name: "abc",
-        Course: "B-Tech"
+        id : 1,
+        name : "abc"
     },
     {
-        id: 2,
-        Name: "xyz",
-        Course: "MBA"
+        id : 2,
+        name : "xyz"
     }
 ]
-
-// Data Read
-app.get('/read', (req, res) => {
-    try{
-        res.status(200).json({message: "Show All Data", data: datas});
-    } catch(err){
-        res.status(500).json({message: "Data Not Found", error: err.message})
+// app.get('/read', (req, res) => {
+//         res.json(students);
+    
+// })
+app.get('/read/:id', (req, res) => {
+    const id = req.params.id;
+    const student = students.find(item => item.id == id);
+    if (!student){
+        return res.status(404).json("Data Not Found");
     }
+    res.status(200).json(student);
 })
-
-//Data Read By Id
-// This Process is Known as Params
-app.get('/about/:id', (req, res) => {
-    try{
-        const id = req.params.id;
-        const data = datas.find(item => item.id == id);
-        if (!data){
-            res.status(404).json({message: "Data Not Fount", d : datas});
-        } else {
-            res.status(200).json({message: "Data Found", data});
-        }
-    } catch(err) {
-        res.status(500).json({message: "Data Not Fount", error: err.message});
-    }
-})
-
-//To Create Data app.post Method is used
-//Data Created
 app.use(express.json());
-app.post('/create', (req, res) => {
-    try{
-        const newData ={
-            id: datas.length + 1,
-            ...req.body
-        }
-        datas.push(newData);
-        res.status(200).json({message: "Data Added", newData});
-    } catch(err){
-        res.status(500).json({message: "Data Not Fount", error: err.message});
-    }
+app.post('/add', (req, res) => {
+    const newData = {
+        id: students.length + 1,
+        ...req.body
+    };
+    students.push(newData);
+    res.json({message: "data Added", students});
 })
-
-//To Update Data app.put Method is Used
-//Data Update
-app.put('/update/:id', (req, res) => {
-    try{
-        const id = req.params.id;
-        const data = datas.findIndex(item => item.id == id);
-        if (data === -1){
-            return res.status(404).json({message: "Data Not Found"})
-        }
-        datas[data] = {
-            ...datas[data],
-            ...req.body
-        }
-        res.status(200).json({message: "Data Updated", d: datas[data]});   
-    } catch (err){
-        res.status(500).json({message: "Data Not Found", error: err.message});
-    }
-})
-
-//To Delete Data app.delete Method is Used
-//Data Delete
 app.delete('/delete/:id', (req, res) => {
-    try{
-        const id = req.params.id;
-        const data = datas.find(item => item.id == id);
-        if (data == -1){
-            return res.status(404).json({message: "Data Not Found"});
-        }
-        datas.splice(data, 1);
-        res.status(200).json({message: "Data Deleted", data});
-    } catch(err){
-        res.status(500).json({message: "Data Not Found", error: err.message});
-    }
+    const id = req.params.id;
+    const data = students.find(s => s.id == id);
+    if (data == -1) return res.json("Data Not Found");
+    students.splice(data, 1);
+    res.json({message:"Data Deleted", students});
 })
-
 app.listen(port, () => {
     console.log(`Server is Running at http://localhost:${port}`);
-});
+})
